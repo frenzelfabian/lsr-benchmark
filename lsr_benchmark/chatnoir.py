ae94951752d547c2f1fd9ef60918e4c986d4c474
Maik Fröbe <maik.froebe@uni-jena.de>
2025-11-13 09:18:55 +0100

    add caching for chatnori

--- lsr_benchmark/chatnoir.py
+++ lsr_benchmark/chatnoir.py
@@ -1,18 +1,49 @@
 from pathlib import Path
 from ir_datasets.formats import BaseDocs
+from ir_datasets.indices.base import Docstore
 from ir_datasets import Dataset
 from ir_datasets.formats import TrecQrels, TrecQueries
 from ir_datasets.util import StringFile
 from ir_datasets import registry
+import json
+import gzip
+
+def cached_chatnoir_docs_store(chatnoir_index: str, cache_file: Path):
+    from chatnoir_api.irds import ChatNoirDocsStore, ChatNoirOwiDoc
+    docs_store = ChatNoirDocsStore(chatnoir_index)
+    doc_id_to_doc = {}
+
+    with gzip.open(cache_file, "rt") as f:
+        for l in f:
+            try:
+                l = json.loads(l)
+            except:
+                continue
+            doc_id_to_doc[l["doc_id"]] = ChatNoirOwiDoc(l["doc_id"], l["text"], l["url"], l["main_content"], l["title"], l["description"])
+            
+
+    class CachedChatNoirDocsStore(Docstore):
+        def get_many_iter(self, doc_ids):
+            for doc in doc_ids:
+                if doc in doc_id_to_doc:
+                    yield doc_id_to_doc[doc]
+                else:            
+                    with gzip.open(cache_file, "at") as f:
+                        ret = docs_store.get(doc)
+                        f.write(json.dumps(ret._asdict()) + "\n")
+                        f.flush()
+                        yield ret
+
+    return CachedChatNoirDocsStore(ChatNoirOwiDoc)
 
 def register_subsample_from_chatnoir(chatnoir_index: str, qrels_file: Path, topics_file: Path, ir_datasets_id: str):
     if ir_datasets_id in registry:
         return
 
-    from chatnoir_api.irds import ChatNoirDocsStore
+    
     class ChatNoirDocs(BaseDocs):
         def docs_store(self):
-            return ChatNoirDocsStore(chatnoir_index)
+            return cached_chatnoir_docs_store(chatnoir_index, Path(qrels_file).parent.parent / "chatnoir-docs.jsonl.gzip")
     docs = ChatNoirDocs()
     qrels = TrecQrels(StringFile(open(qrels_file, "r").read()), {})
     topics = TrecQueries(StringFile(open(topics_file, "r").read()))
